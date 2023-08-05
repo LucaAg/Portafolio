6894f9a6b92f901c981d64d27950a0a6d4b654c1
Luca <nahuquilmes2000@gmail.com>
2023-08-05 06:06:14 -0300

    Iconos agregados, Github stats y copyright

--- src/app/paginas/bienvenida/bienvenida.component.ts
+++ src/app/paginas/bienvenida/bienvenida.component.ts
@@ -7,10 +7,25 @@ import { Component } from '@angular/core';
 })
 export class BienvenidaComponent {
   spinner:boolean = false;
-
+  frames:any;
+  lenguajes:any;
+  mensajeEstandar:any;
+  baseDatos:any;
+  otros:any;
+  estilos:any;
   constructor()
   {
     this.activarSpinner();
+  }
+
+  ngOnInit()
+  {
+    this.frames = document.getElementById('frameworks');
+    this.mensajeEstandar = document.getElementById('no-data');
+    this.lenguajes = document.getElementById('lenguajes');
+    this.baseDatos = document.getElementById('base-datos');
+    this.otros = document.getElementById('otros');
+    this.estilos = document.getElementById('estilos');
   }
 
   activarSpinner()
@@ -23,6 +38,58 @@ export class BienvenidaComponent {
 
   descargarCV()
   {
-    console.log("Descargar CV");
+    let nombreArchivo = 'Agnoli-Luca-Nahuel-CV-Programador-FullStack.pdf';
+    let url = '../../../assets/LucaAgnoli CV.pdf';
+    const link = document.createElement('a');
+    link.href= url;
+    link.target = '_blank';
+    link.download = nombreArchivo;
+    link.click();
+  }
+
+  onEntraMouse(datosAMostrar:string)
+  {
+    this.mensajeEstandar.classList.add("esconder");
+    switch(datosAMostrar)
+    {
+      case "frames":
+        this.frames.classList.remove("esconder");
+        break;
+      case "lenguajes":
+        this.lenguajes.classList.remove("esconder");
+        break;
+      case "otros":
+        this.otros.classList.remove("esconder");
+        break;
+      case "base-datos":
+        this.baseDatos.classList.remove("esconder");
+        break;
+      case "estilos":
+        this.estilos.classList.remove("esconder");
+        break;        
+    }
+  }
+
+  onSaleMouse(datoAEsconder:string)
+  {
+    switch(datoAEsconder)
+    {
+      case "frames":
+        this.frames.classList.add("esconder");
+        break;
+      case "lenguajes":
+        this.lenguajes.classList.add("esconder");
+        break;
+      case "otros":
+        this.otros.classList.add("esconder");
+        break;
+      case "base-datos":
+        this.baseDatos.classList.add("esconder");
+        break;
+      case "estilos":
+        this.estilos.classList.add("esconder");
+        break;        
+    }
+    this.mensajeEstandar.classList.remove("esconder");
   }
 }
